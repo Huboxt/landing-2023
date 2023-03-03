7238241246dacbdb9ed4982f333a0682f574445c
Andriy Yurchuk <paltashka50@gmail.com>
2023-03-03 17:42:50 +0200

    fix review

--- src/components/Clutch/clutches.js
+++ src/components/Clutch/clutches.js
@@ -5,28 +5,28 @@ import viaBlock from '../../assets/img/industrieIcons/viaBlock.svg';
 export const clutches = [
   {
     stars: 5,
-    description: 'Aside from providing superior products, their ability to genuinely interact on a personal level impresses us the most.',
+    description: 'Huboxt didn’t just take a design and develop a working product. They setup a collaborative environment and made it their mission to take our original ideas and make them better. Aside from providing superior products, their ability to genuinely interact on a personal level impresses us the most.',
     image: null,
     projectName: "NuVision Investments",
     projectDescription: "Custom Software Development for Tech Startup"
   },
   {
     stars: 5,
-    description: 'They were flexible and open-minded.',
+    description: 'Huboxt is a modern new wave company without a lot of decision-making levels and bureaucracy Our ideas and improvements were picked up and processed in a timely manner. They were flexible and open-minded.',
     image: datBi,
     projectName: "DatBI",
     projectDescription: "Web Development & Branding for Software Company"
   },
   {
     stars: 5,
-    description: 'Their ability to take an idea and turn it into reality stood out.\nteam is efficient, productive, and on top of the ball. Huboxt excels in turning discrete ideas into tangible designs.',
+    description: 'Their ability to take an idea and turn it into reality stood out. This project came in a few different phases. Huboxt was able take our idea and turn it into a design.\nThe team is efficient, productive, and on top of the ball. Huboxt excels in turning discrete ideas into tangible designs.',
     image: null,
     projectName: "Digital Notion",
     projectDescription: "Performance Marketing Leads Platform"
   },
   {
     stars: 5,
-    description: 'They are very creative and engaged, doing software is their passion!',
+    description: 'They are very creative and engaged, doing software is their passion! Impression is very good. Web is looking really modern and professional. Everything works smoothly. User interface - easy and friendly. Good job!',
     image: goes,
     projectName: "GOES GmbH & Prime WSI",
     projectDescription: "DMS Cloud for Oil & Gas Equipment Manufacturer"
