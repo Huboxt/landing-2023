7d835f1d51a34553254a374a825d946ea6679e04
OleksandrRomanov <vamromanov@gmail.com>
2023-09-29 10:06:26 +0200

    data concept improvements

--- src/components/GetTouch/GetTouch.jsx
+++ src/components/GetTouch/GetTouch.jsx
@@ -4,13 +4,10 @@ import getInTouchBg from '../../assets/img/getInTouchBg.svg';
 import ContactFormComponent from '../ContactForm/contact-form.component';
 import style from './getTouch.module.css';
 
-const GetTouch = ({ borderRadius }) => {
-  const wrapperStyle = {
-    borderRadius: borderRadius || 'unset', 
-  };
+const GetTouch = ({  }) => {
 
   return (
-    <div className={style.getTouchWrapper} style={wrapperStyle} id="getInTouch">
+    <div className={style.getTouchWrapper} id="getInTouch">
       <Image src={getInTouchBg} alt=" " className={style.getTouchIcon} />
       <div className={style.container}>
         <div className={style.title}>Contact us if you have any projects in mind</div>
--- src/pages/projects/data-concept.js
+++ src/pages/projects/data-concept.js
@@ -15,6 +15,9 @@ import dataConceptTitle from "./dataConceptTitle.svg";
 import dataConceptJobs1 from "./data-concept-jobs1.svg";
 import dataConceptJobs2 from "./data-concept-jobs2.svg";
 import dataConceptJobs3 from "./data-concept-jobs3.svg";
+import numberIcon1 from "../../assets/img/numberIcon1.svg";
+import numberIcon2 from "../../assets/img/numberIcon2.svg";
+import numberIcon3 from "../../assets/img/numberIcon3.svg";
 import { Parallax, ParallaxProvider } from "react-scroll-parallax";
 
 const DataConceptPage = () => {
@@ -52,7 +55,9 @@ const DataConceptPage = () => {
             I NEED THE SAME!
           </button>
         </div>
-        <div  className={`${style.stackContainer} ${style.dataConceptStackContainer}`}>
+        <div
+          className={`${style.stackContainer} ${style.dataConceptStackContainer}`}
+        >
           <div className={style.stackBlock}>
             <Image
               src={golangIcon}
@@ -78,12 +83,14 @@ const DataConceptPage = () => {
           </video>
         </Parallax>
         <Parallax
-          startScroll={800}
-          endScroll={1500}
-          translateY={["50px", "-200px"]}
+          // startScroll={800}
+          // endScroll={1500}
+          // translateY={["50px", "-200px"]}
         >
-          <div className={`${style.aboutContainer} ${style.dataConceptAboutMargin}`}>
-          <div className={style.table}>
+          <div
+            className={`${style.aboutContainer} ${style.dataConceptAboutMargin}`}
+          >
+            <div className={style.table}>
               <div>
                 <div className={style.tableTitle}>CLIENT</div>
                 <div className={style.tableDescr}>Dataconcepts</div>
@@ -119,59 +126,38 @@ const DataConceptPage = () => {
               translateX={["800px", "0px"]}
               className={style.staticParallax}
             >
-              <div className={style.aboutCard}>
-                <div className={style.aboutBlockTitle}>UI/UX Design</div>
+              <div className={style.cardWithoutBorder}>
                 <div className={style.aboutBlockDescr}>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-                  irure dolor in reprehenderit in voluptate velit esse cillum
-                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                  cupidatat non proident, sunt in culpa qui officia deserunt
-                  mollit anim id est laborum.
-                </div>
-              </div>
-            </Parallax>
-            <Parallax
-              startScroll={200}
-              endScroll={1500}
-              translateX={["800px", "0px"]}
-              className={style.staticParallax}
-            >
-              <div className={style.aboutCard}>
-                <div className={style.aboutBlockTitle}>Development</div>
-                <div className={style.aboutBlockDescr}>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-                  irure dolor in reprehenderit in voluptate velit esse cillum
-                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                  cupidatat non proident, sunt in culpa qui officia deserunt
-                  mollit anim id est laborum.
+                  The customer is a digital marketing agency. They use their own
+                  custom CRM for newsletter through SMS and Email and a data
+                  aggregation system which was sourcing it via API. Once they
+                  started a fruitful partnership with data collection agencies,
+                  they got so much data, and it caused target CRM performance
+                  issues with inability to handle about 2 millions requests per
+                  day. <br /> <br />
+                  We’ve come up as a dedicated team to build a V2 data
+                  aggregation service, which would be as a middle layer enricher
+                  between agencies data and customer’s CRM.
                 </div>
               </div>
             </Parallax>
           </div>
         </Parallax>
         <Parallax
-          startScroll={1300}
-          endScroll={2000}
-          translateY={["-100px", "-300px"]}
+        // startScroll={1300}
+        // endScroll={2000}
+        // translateY={["-100px", "-300px"]}
         >
-          <div className={style.researchContainer}>
+          <div className={style.aboutContainer}>
             <Parallax
               startScroll={1500}
               endScroll={2000}
               translateX={["-800px", "0px"]}
               className={style.staticParallax}
             >
-              <Image
-                src={researchTitle}
-                alt="researchTitle"
-                style={{ width: "70%", height: "70%" }}
-              />
+              <div className={style.whatCanDoTitle}>
+                THE <br /> CHALLENGE
+              </div>
             </Parallax>
             <Parallax
               startScroll={300}
@@ -179,44 +165,77 @@ const DataConceptPage = () => {
               translateX={["800px", "50px"]}
               className={style.staticParallax}
             >
-              <div className={style.researchCard}>
-                <div className={style.researchBlockTitle}>Problem</div>
-                <div className={style.researchBlockDescr}>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-                  irure dolor in reprehenderit in voluptate velit esse cillum
-                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                  cupidatat non proident, sunt in culpa qui officia deserunt
-                  mollit anim id est laborum.
+              <div className={style.cardWithoutBorder}>
+                <div className={style.aboutBlockDescr}>
+                  They asked us to keep their application’s business logic, but
+                  make it generic and scalable. It came up with the custom data
+                  workflow configurator service, which allows to manage incoming
+                  data, store it, enrich and push to target endpoints, and not
+                  stick to specific customer’s CRM. Rework architecture to make
+                  it fast, scalable, less demanding to achieve stability and
+                  reduce expenses on infrastructure.
                 </div>
               </div>
             </Parallax>
-            <Parallax
-              startScroll={400}
-              endScroll={2100}
-              translateX={["800px", "50px"]}
-              className={style.staticParallax}
-            >
-              <div className={style.researchCard}>
-                <div className={style.researchBlockTitle}>Solution</div>
-                <div className={style.researchBlockDescr}>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-                  irure dolor in reprehenderit in voluptate velit esse cillum
-                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                  cupidatat non proident, sunt in culpa qui officia deserunt
-                  mollit anim id est laborum.
+            <div className={style.requirementsCards}>
+              <Parallax
+              // startScroll={700}
+              // endScroll={2800}
+              // translateX={["800px", "50px"]}
+              >
+                <div className={style.requirementsCard}>
+                  <Image
+                    src={numberIcon1}
+                    alt="numberIcon1"
+                    className={style.numberIcon1}
+                  />
+                  <div className={style.aboutBlockTitle}>Approach</div>
+                  <div className={style.aboutBlockDescr}>
+                    Their approach and architecture was not optimal, so it was
+                    hard to adjust, slow and resource-demanding
+                  </div>
                 </div>
-              </div>
-            </Parallax>
+              </Parallax>
+              <Parallax
+              // startScroll={800}
+              // endScroll={2900}
+              // translateX={["800px", "50px"]}
+              >
+                <div className={style.requirementsCard}>
+                  <Image
+                    src={numberIcon2}
+                    alt="numberIcon2"
+                    className={style.numberIcon}
+                  />
+                  <div className={style.aboutBlockTitle}>Cost</div>
+                  <div className={style.aboutBlockDescr}>
+                    The cloud AWS architecture cost was about 6000$/month
+                  </div>
+                </div>
+              </Parallax>
+              <Parallax
+              // startScroll={800}
+              // endScroll={2900}
+              // translateX={["800px", "50px"]}
+              >
+                <div className={style.requirementsCard}>
+                  <Image
+                    src={numberIcon3}
+                    alt="numberIcon3"
+                    className={style.numberIcon}
+                  />
+                  <div className={style.aboutBlockTitle}>Database</div>
+                  <div className={style.aboutBlockDescr}>
+                    AWS lambdas were wrongly used, the database was not properly
+                    normalized and indexed
+                  </div>
+                </div>
+              </Parallax>
+            </div>
           </div>
         </Parallax>
-        <Parallax >
-          <div className={style.dataConceptCardsMargin}>
+        <Parallax>
+          <div className={style.researchContainer}>
             <Image
               src={dataConceptCard}
               id="dataConceptCard"
@@ -228,6 +247,183 @@ const DataConceptPage = () => {
               }}
             />
           </div>
+        </Parallax>
+
+        <Parallax
+        // startScroll={1300}
+        // endScroll={2000}
+        // translateY={["-100px", "-300px"]}
+        >
+          <div className={style.aboutContainer}>
+            <Parallax
+              startScroll={1500}
+              endScroll={2000}
+              translateX={["-800px", "0px"]}
+              className={style.staticParallax}
+            >
+              <div className={style.whatCanDoTitle}>DEVELOPMENT</div>
+            </Parallax>
+            <Parallax
+              startScroll={300}
+              endScroll={2000}
+              translateX={["800px", "50px"]}
+              className={style.staticParallax}
+            >
+              <div className={style.cardWithoutBorder}>
+                <div className={style.aboutBlockDescr}>
+                  We decided to move from AWS to a dedicated server to gain more
+                  freedom in terms of budget and customization. We used Goland
+                  and MySQL to develop a microservice-based scalable backend. We
+                  chose Golang to maximize server hardware utilization through
+                  concurrency and multithreading combined with load and stress
+                  tests are really good to maximize code optimization. We
+                  achieve flawless code work through a number of stress tests
+                  and code improvements.
+                </div>
+              </div>
+            </Parallax>
+            <div className={style.requirementsCards}>
+              <Parallax
+              // startScroll={700}
+              // endScroll={2800}
+              // translateX={["800px", "50px"]}
+              >
+                <div className={style.requirementsCard}>
+                  <Image
+                    src={numberIcon1}
+                    alt="numberIcon1"
+                    className={style.numberIcon1}
+                  />
+                  <div className={style.aboutBlockTitle}>
+                    Backend capability
+                  </div>
+                  <div className={style.aboutBlockDescr}>
+                    The first challenge was to make a backend capable of getting
+                    data from 2+ millions requests per day,
+                    filtering/enriching/operating data of 200+ millions entries,
+                    balancing upload and download using different strategies and
+                    workflows.
+                  </div>
+                </div>
+              </Parallax>
+              <Parallax
+              // startScroll={800}
+              // endScroll={2900}
+              // translateX={["800px", "50px"]}
+              >
+                <div className={style.requirementsCard}>
+                  <Image
+                    src={numberIcon2}
+                    alt="numberIcon2"
+                    className={style.numberIcon}
+                  />
+                  <div className={style.aboutBlockTitle}>
+                    Transfer & convert
+                  </div>
+                  <div className={style.aboutBlockDescr}>
+                    The second challenge was to transfer and convert ~500
+                    millions records to common db structure.
+                  </div>
+                </div>
+              </Parallax>
+              <Parallax
+              // startScroll={800}
+              // endScroll={2900}
+              // translateX={["800px", "50px"]}
+              >
+                <div className={style.requirementsCard}>
+                  <Image
+                    src={numberIcon3}
+                    alt="numberIcon3"
+                    className={style.numberIcon}
+                  />
+                  <div className={style.aboutBlockTitle}>Migrate data</div>
+                  <div className={style.aboutBlockDescr}>
+                    Third challenge to migrate legacy data flow logic to a new
+                    generic workflow structure.
+                  </div>
+                </div>
+              </Parallax>
+            </div>
+          </div>
+        </Parallax>
+
+        <Parallax
+        // startScroll={1300}
+        // endScroll={2000}
+        // translateY={["-100px", "-300px"]}
+        >
+          <div className={style.aboutContainer}>
+            <Parallax
+              startScroll={1500}
+              endScroll={2000}
+              translateX={["-800px", "0px"]}
+              className={style.staticParallax}
+            >
+              <div className={style.whatCanDoTitle}>IMPACT</div>
+            </Parallax>
+            <div className={style.requirementsCards}>
+              <Parallax
+              // startScroll={700}
+              // endScroll={2800}
+              // translateX={["800px", "50px"]}
+              >
+                <div className={style.requirementsCard}>
+                  <Image
+                    src={numberIcon1}
+                    alt="numberIcon1"
+                    className={style.numberIcon1}
+                  />
+                  <div className={style.aboutBlockTitle}>Reduced expences</div>
+                  <div className={style.aboutBlockDescr}>
+                    We reduced infrastructure expenses 30 times from 6000$ to
+                    200$ moving from AWS to dedicated server and optimizing
+                    code.
+                  </div>
+                </div>
+              </Parallax>
+              <Parallax
+              // startScroll={800}
+              // endScroll={2900}
+              // translateX={["800px", "50px"]}
+              >
+                <div className={style.requirementsCard}>
+                  <Image
+                    src={numberIcon2}
+                    alt="numberIcon2"
+                    className={style.numberIcon}
+                  />
+                  <div className={style.aboutBlockTitle}>More transparency</div>
+                  <div className={style.aboutBlockDescr}>
+                    We added more transparency to data by adding advanced stats,
+                    multi criteria filters, logs.
+                  </div>
+                </div>
+              </Parallax>
+              <Parallax
+              // startScroll={800}
+              // endScroll={2900}
+              // translateX={["800px", "50px"]}
+              >
+                <div className={style.requirementsCard}>
+                  <Image
+                    src={numberIcon3}
+                    alt="numberIcon3"
+                    className={style.numberIcon}
+                  />
+                  <div className={style.aboutBlockTitle}>Further scalling</div>
+                  <div className={style.aboutBlockDescr}>
+                    We made further scaling much easier due to optimal
+                    microservice architecture.
+                  </div>
+                </div>
+              </Parallax>
+            </div>
+          </div>
+        </Parallax>
+
+        <Parallax>
+        <div className={style.researchContainer}>
           <div>
             <Image
               src={dataConceptJobs1}
@@ -272,11 +468,12 @@ const DataConceptPage = () => {
               }}
             />
           </div>
+          </div>
         </Parallax>
       </ParallaxProvider>
       <div>
         <div className={style.getTouchWrapper}>
-          <GetTouch borderRadius="60px 60px 0px 0px" />
+          <GetTouch />
           <Footer />
         </div>
       </div>
