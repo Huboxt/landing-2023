d27d81176f1e6e3acad9d2881279c831d2588b8b
OleksandrRomanov <vamromanov@gmail.com>
2023-10-03 09:15:39 +0200

    tags added

--- src/components/OurWorks/project.js
+++ src/components/OurWorks/project.js
@@ -15,7 +15,8 @@ export const projects = [
     description: "It is how we helped to hit the market fast. It took 2 months from just an idea of the app to App Store and Google Play publication. Petmate.ai is a chat-like mobile application that answers any question about keeping, training and treating your pet.",
     folder: 'projects',
     Image: petmateCard,
-    page: "petmate"
+    page: "petmate",
+    tags: "design, development, GPTChat integration"
   },
   {
     id: '2',
@@ -23,7 +24,8 @@ export const projects = [
     description: "This is how we launched an early version of a SaaS product and made it comply with the United States Postal Service regulations. UploadLetters is an app where you submit your PDF document online in a few clicks and your partner receives physical mail in an envelope delivered by USPS.",
     folder: 'projects',
     Image: uploadLettersCard,
-    page: "uploadletters"
+    page: "uploadletters",
+    tags: "design, development, USPS integration, OCR"
   },
   {
     id: '3',
@@ -31,41 +33,47 @@ export const projects = [
     description: "Here is an example of how we scaled an application that unpredictably grew much faster than expected. Dataconcept is a middle-layer data aggregation service for a marketing agency that handles more than 2 million requests per day.",
     Image: DataConceptCard,
     folder: 'projects',
-    page: "data-concept"
+    page: "data-concept",
+    tags: "design, development, management"
   },
   {
     id: '4',
     title: "Segma", 
     description: "The fast changing world of web development has expanded our skill set and has made us experienced in all major ui frameworks.",
     Image: SegmaCard,
-    page: "segma"
+    page: "segma",
+    tags: "design, development, management"
   },
   {
     id: '5',
     title: "FleetInsider", 
     description: "The fast changing world of web development has expanded our skill set and has made us experienced in all major ui frameworks, which makes your application look nice and modern while being and fast.",
     Image: fleetInsiderCard,
-    page: "fleetInsider"
+    page: "fleetInsider",
+    tags: "design, development, management"
   },
   {
     id: '6',
     title: "Car Booking service", 
     description: "The fast changing world of web development...",
     Image: cardBookingCard,
-    page: "carbookingservice"
+    page: "carbookingservice",
+    tags: "design, development, management"
   },
   {
     id: '7',
     title: "DMS/MMS", 
     description: "The fast changing world of web development has expanded our skill set and has made us experienced in all major ui frameworks, which makes your application look nice and modern while being and fast.",
     Image: DMSCard,
-    page: "dms"
+    page: "dms",
+    tags: "design, development, management"
   },
   {
     id: '8',
     title: "Datbi", 
     description: "The fast changing world of web development has expanded our skill set and has made us experienced in all major ui",
     Image: DatBiCard,
-    page: "datbi"
+    page: "datbi",
+    tags: "design, development, management"
   }
 ]
--- src/pages/our-works/our-works-page.js
+++ src/pages/our-works/our-works-page.js
@@ -1,46 +1,62 @@
-import React from 'react';
-import Header from '@/components/Header/Header';
-import GetTouch from '@/components/GetTouch/GetTouch';
-import Footer from '@/components/Footer/Footer';
-import { projects } from '@/components/OurWorks/project';
+import React from "react";
+import Header from "@/components/Header/Header";
+import GetTouch from "@/components/GetTouch/GetTouch";
+import Footer from "@/components/Footer/Footer";
+import { projects } from "@/components/OurWorks/project";
 import Image from "next/image";
-import ourWorksIcon from '../../assets/img/ourWorksIcon.svg';
-import style from "@/pages/our-works/ourWorksPage.module.css"
-import touch from '../../assets/img/getInTouch.svg';
+import ourWorksIcon from "../../assets/img/ourWorksIcon.svg";
+import style from "@/pages/our-works/ourWorksPage.module.css";
+import touch from "../../assets/img/getInTouch.svg";
 import Link from "next/link";
-import { useRouter } from 'next/router';
-
+import { useRouter } from "next/router";
 
 const OurWorksPage = () => {
   const router = useRouter();
 
   return (
     <div className={style.container}>
-      <Header showGetTouchBtn/>  
+      <Header showGetTouchBtn />
       <div>
-        <Image src={ourWorksIcon} alt="ourWorksIcon" className={style.ourWorksIcon}/>
+        <Image
+          src={ourWorksIcon}
+          alt="ourWorksIcon"
+          className={style.ourWorksIcon}
+        />
       </div>
       <div>
-      {
-            projects.map((e, index) => {
-              const isExcludedPage = ["segma", "fleetInsider", "carbookingservice", "dms", "datbi"].includes(e.page);
-              return (
-                <div key={e.id} className={style.Wrapper}>
-                  <div className={style.flipCardInner}>
-                    <div className={style.projectBack} key={e.name + index}>
-                      <div className={style.flex}>
-                        <div className={style.leftBlock}>
-                          <button className={style.projectButtonBack}>
-                            DESIGN
-                          </button>
-                          <div className={style.projectTitleBack}>
-                            {e.title}
-                          </div>
-                          <div className={style.projectDescriptionBack}>
-                            {e.description}
-                          </div>
-                          {!isExcludedPage && (
-                        <button className={style.projectButtonBackDev} onClick={() => router.push(`/${e.folder}/${e.page}`)}>
+        {projects.map((e, index) => {
+          const isExcludedPage = [
+            "segma",
+            "fleetInsider",
+            "carbookingservice",
+            "dms",
+            "datbi",
+          ].includes(e.page);
+          const tagsArray = e.tags.split(", "); // Розділити рядок тегів на масив
+          return (
+            <div key={e.id} className={style.Wrapper}>
+              <div className={style.flipCardInner}>
+                <div className={style.projectBack} key={e.name + index}>
+                  <div className={style.flex}>
+                    <div className={style.leftBlock}>
+                      {/* Мапимо кнопки для кожного тега */}
+                      {tagsArray.map((tag, tagIndex) => (
+                        <button
+                          key={tagIndex}
+                          className={style.projectButtonBack}
+                        >
+                          {tag}
+                        </button>
+                      ))}
+                      <div className={style.projectTitleBack}>{e.title}</div>
+                      <div className={style.projectDescriptionBack}>
+                        {e.description}
+                      </div>
+                      {!isExcludedPage && (
+                        <button
+                          className={style.projectButtonBackDev}
+                          onClick={() => router.push(`/${e.folder}/${e.page}`)}
+                        >
                           <Image
                             src={touch}
                             alt="get"
@@ -49,57 +65,20 @@ const OurWorksPage = () => {
                           VIEW PROJECT
                         </button>
                       )}
-                        </div>
-                        <div className={style.projectImage}>
-                          <Image
-                            src={e.Image}
-                            alt=" "
-                            style={{ width: "100%", height: "100%" }}
-                          />
-                        </div>
-                      </div>
                     </div>
-                  </div>
-
-                  <div className={style.flipCardInnerMobile}>
-                    <div className={style.projectBack} key={e.name + index}>
-                      <div>
-                        <div className={style.projectTitleBack}>{e.title}</div>
-                        <div className={style.projectDescriptionBack}>
-                          {e.description}
-                        </div>
-                      </div>
-                      <div className={style.projectImage}>
-                        <Image
-                          src={e.Image}
-                          alt=" "
-                          style={{ width: "100%", height: "100%" }}
-                        />
-                      </div>
-                      <div className={style.projectButtons}>
-                        <button className={style.projectButtonBack}>
-                          <Image
-                            src={touch}
-                            alt="get"
-                            className={style.getInTouchImg}
-                          />
-                          DESIGN
-                        </button>
-                        <button className={style.projectButtonBack}>
-                          <Image
-                            src={touch}
-                            alt="get"
-                            className={style.getInTouchImg}
-                          />
-                          VIEW PROJECT
-                        </button>
-                      </div>
+                    <div className={style.projectImage}>
+                      <Image
+                        src={e.Image}
+                        alt=" "
+                        style={{ width: "100%", height: "100%" }}
+                      />
                     </div>
                   </div>
                 </div>
-              );
-            })
-          }
+              </div>
+            </div>
+          );
+        })}
       </div>
       <GetTouch borderRadius="60px 60px 0px 0px" />
       <Footer />
